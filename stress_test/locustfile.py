def1bbb13094d763cb994028dc2411b4bf3c7065
mlopez0 <m.lopez@innopolis.university>
2023-06-07 18:21:49 -0300

    fix small errors

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -6,5 +6,27 @@ class APIUser(HttpUser):
 
     # Put your stress tests here.
     # See https://docs.locust.io/en/stable/writing-a-locustfile.html for help.
-    # TODO
-    raise NotImplementedError
+    # TODO ✅
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def feedback(self):
+        data = {
+            "report": "{'filename': 'test', 'prediction': 'test-pred', 'score': 1. }"
+        }
+
+        self.client.post("/feedback", data=data)
+
+    @task
+    def predict(self):
+        files = [("file", ("dog.jpeg", open("dog.jpeg", "rb"), "image/jpeg"))]
+        headers = {}
+        payload = {}
+        self.client.post(
+            "/predict",
+            data=payload,
+            files=files,
+            headers=headers,
+        )
